da2f09bb4f5986944e146fdf12e1bc98f132c73d
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-03-06 11:39:28 +0330

    feat (src/js/views/View.ts): Add render method to View class to generate and render markup with data

--- src/js/views/View.ts
+++ src/js/views/View.ts
@@ -6,4 +6,11 @@ export default abstract class View {
   constructor(parentElement: HTMLElement) {
     this._parentElement = parentElement;
   }
+
+  render(data: any): void {
+    this._data = data;
+    const markup = this._generateMarkup();
+    this._parentElement.innerHTML = "";
+    this._parentElement.insertAdjacentHTML("afterbegin", markup);
+  }
 }
