16c952f00a2874d5214ba96d8cc45214584fdbd8
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-03-06 11:10:15 +0330

    feat(src/index.ts): Create main entry point for MVC architecture project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 import "./styles/index.css";
+import RecipeController from "./js/controllers/controller";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const recipeController = new RecipeController();
+});
