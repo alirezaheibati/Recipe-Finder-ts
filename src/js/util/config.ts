fabf65d6f2ef943924231e9b3dce213cda3ae963
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-03-06 11:06:38 +0330

    config (src/js/util/config.ts): Add API key and base URL for Spoonacular API

---
+++ src/js/util/config.ts
@@ -0,0 +1,9 @@
+/**
+ * The API key for accessing the Spoonacular API.
+ */
+export const API_KEY: string = "cf02679fbc7640dab5b33c1d54e006f6";
+/**
+ * The base URL for the Spoonacular API.
+ * All API requests will be made to this endpoint.
+ */
+export const API_URL: string = "https://api.spoonacular.com/recipes/";
