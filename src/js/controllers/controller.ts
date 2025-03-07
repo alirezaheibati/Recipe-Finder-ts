786218531d9eac2cdcfca6f2536f8675f286c809
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-03-07 10:03:25 +0330

    feat (src/js/controllers/controller.ts): add _getRandomRecipeController method to handle random recipe fetching and rendering

--- src/js/controllers/controller.ts
+++ src/js/controllers/controller.ts
@@ -1,9 +1,32 @@
 import RecipeModel from "../models/model";
+import ResultsView from "../views/resultsView";
+
 export default class RecipeController {
   recipeModel: RecipeModel;
+  resultsView: ResultsView;
   constructor() {
     this.recipeModel = new RecipeModel();
+    this.resultsView = new ResultsView();
+
     this.setupEventHandlers();
+    this._getRandomRecipeController();
   }
+
   setupEventHandlers(): void {}
+
+  _getRandomRecipeController = async function () {
+    try {
+      await this.recipeModel.getRandomRecipes();
+      if (this.recipeModel.searchResults.length === 0) {
+        throw new Error(
+          "Something went wrong. Please search for your own desired recipe"
+        );
+      }
+      this.resultsView.render(this.recipeModel.searchResults);
+    } catch (err) {
+      console.log(err);
+
+      console.log("some thing went wrong");
+    }
+  };
 }
