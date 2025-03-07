ee17189387c7716315879886692ce9cfddea9cc4
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-03-07 10:01:59 +0330

    feat (src/js/models/model.ts): Add getRandomRecipes method to fetch random recipes on app start

--- src/js/models/model.ts
+++ src/js/models/model.ts
@@ -1,3 +1,17 @@
+import { getJSON } from "../util/helper";
+import { API_KEY, API_URL } from "../util/config";
+import { Recipe } from "../../interfaces/Recipe";
 export default class RecipeModel {
-  constructor() {}
+  public searchResults: Recipe[];
+  constructor() {
+    this.searchResults = [];
+  }
+  getRandomRecipes = async function (): Promise<void> {
+    try {
+      const data = await getJSON(`${API_URL}random?number=5&apiKey=${API_KEY}`);
+      this.searchResults = data.recipes;
+    } catch (err) {
+      throw err;
+    }
+  };
 }
