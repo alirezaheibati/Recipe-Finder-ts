f7b72f028e10a0e9135bb09d3dca9191c9dddd2e
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-03-09 09:10:42 +0330

    feat (src/js/views/bookmarkView.ts): add bookmarkedRecipesBtnClickHandler method for handling bookmark button clicks

--- src/js/views/bookmarkView.ts
+++ src/js/views/bookmarkView.ts
@@ -3,4 +3,11 @@ export default class BookmarkView {
   constructor() {
     this._parentElement = document.getElementById("view-bookmarks-btn");
   }
+
+  bookmarkedRecipesBtnClickHandler(handler: () => void) {
+    this._parentElement.addEventListener("click", () => {
+      this._parentElement.classList.toggle("text-red-500");
+      handler();
+    });
+  }
 }
